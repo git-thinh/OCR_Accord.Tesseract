7fb3a95a1b766c56212472eb543c87ff6b8ce2c1
nguyenvanthinh <nguyenvanthinh@f88.vn>
2019-12-27 12:10:13 +0700

    demo

--- demo/Program.cs
+++ demo/Program.cs
@@ -1,15 +1,124 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Accord.Imaging;
+using Accord.Imaging.Filters;
+using System;
+using System.Diagnostics;
+using System.Drawing;
+using Tesseract;
 
 namespace demo
 {
     class Program
     {
+        static Bitmap f_imageFilter(string pathImage)
+        {
+            Stopwatch stopWatch = new Stopwatch();
+            stopWatch.Start();
+            string filename = pathImage.Split('.')[0];
+
+            Bitmap bitmap = Accord.Imaging.Image.FromFile(pathImage);
+
+            Grayscale gray = new Grayscale(0.2125, 0.7154, 0.0721);
+            Bitmap result = gray.Apply(bitmap);
+
+            BradleyLocalThresholding thres = new BradleyLocalThresholding();
+            thres.WindowSize = 80;
+            thres.PixelBrightnessDifferenceLimit = 0.1F;
+            thres.ApplyInPlace(result);
+
+            Sharpen sharp = new Sharpen();
+            sharp.ApplyInPlace(result);
+
+            BilateralSmoothing smooth = new BilateralSmoothing();
+            smooth.KernelSize = 7;
+            smooth.SpatialFactor = 10;
+            smooth.ColorFactor = 60;
+            smooth.ColorPower = 0.5;
+            smooth.ApplyInPlace(result);
+
+            DocumentSkewChecker skew = new DocumentSkewChecker();
+            double angle = skew.GetSkewAngle(result);
+
+            RotateBilinear rot = new RotateBilinear(-angle);
+            rot.FillColor = Color.White;
+            result = rot.Apply(result);
+
+            stopWatch.Stop();
+            f_print(stopWatch);
+
+            result.Save(filename + "_filter.bmp");
+
+            return result;
+        }
+
+        static void f_print(Stopwatch stopWatch)
+        {
+            TimeSpan ts;
+
+            if (stopWatch == null) ts = DateTime.Now.TimeOfDay;
+            else ts = stopWatch.Elapsed;
+
+            string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:000}", ts.Hours, ts.Minutes, ts.Seconds, ts.Milliseconds / 10);
+            Console.WriteLine("Time = " + elapsedTime);
+        }
+
+        static void f_get_id(Bitmap image)
+        {
+            Stopwatch stopWatch = new Stopwatch();
+            stopWatch.Start();
+
+            try
+            {
+                using (var engine = new TesseractEngine(@".\tessdata", "eng", EngineMode.Default))
+                {
+                    //engine.SetVariable("tessedit_char_whitelist", "0123456789:");
+
+                    using (var page = engine.Process(image, Rect.Empty))
+                    //using (var page = engine.Process(image))
+                    {
+                        var text = page.GetText();
+                        stopWatch.Stop();
+                        f_print(stopWatch);
+
+                        Console.WriteLine("Text: {0}", text);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Console.WriteLine(e.ToString());
+            }
+            finally
+            {
+            }
+        }
+
         static void Main(string[] args)
         {
+            string f;
+            f = "1.jpg";
+
+            Bitmap image = f_imageFilter(f);
+            f_get_id(image);
+
+            //Stopwatch stopWatch = new Stopwatch();
+            //stopWatch.Start();
+            //using (var engine = new TesseractEngine(@"./tessdata", "eng", EngineMode.Default))
+            //{
+            //    using (var img = Pix.LoadFromFile(""))
+            //    {
+            //        using (var page = engine.Process(img))
+            //        {
+            //            Console.WriteLine(page.GetText());
+            //        }
+            //    }
+            //}
+            //stopWatch.Stop();
+            //f_print(stopWatch);
+
+            Console.WriteLine("\n\nPress ENTER/RETURN to exit");
+            Console.ReadKey(true);
+
         }
     }
 }
---
+++ demo/TesseractHelper.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Tesseract
+{
+    /// <summary>
+    /// Wrapper around Tesseract Library
+    /// </summary>
+    public static class TesseractHelper
+    {
+        private static Regex regex = new Regex("[^a-zA-Z0-9]");
+
+        /// <summary>
+        /// Gets the number from image.
+        /// </summary>
+        /// <param name="imagePath">The image path.</param>
+        /// <returns></returns>
+        public static int GetNumberFromImage(string imagePath)
+        {
+            var numberValue = 0;
+            if (string.IsNullOrWhiteSpace(imagePath) == false && System.IO.File.Exists(imagePath) == true)
+            {
+                try
+                {
+                    using (var engine = new TesseractEngine(@"./tessdata", "eng", EngineMode.Default))
+                    {
+                        using (var img = Pix.LoadFromFile(imagePath))
+                        {
+                            using (var page = engine.Process(img))
+                            {
+                                var text = page.GetText();
+                                if (string.IsNullOrWhiteSpace(text) == false)
+                                {
+                                    text = regex.Replace(text, string.Empty);                               //remove non alpha numeric characters
+                                    text = text.ToLowerInvariant().Replace('i', '1').Replace('o', '0');    //to fix wrong interpretation
+                                    if (int.TryParse(text, out numberValue) == false)
+                                    {
+                                        Console.WriteLine("Unable to process the file : " + imagePath);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Unexpected Error: " + ex.Message);
+                }
+            }
+            return numberValue;
+        }
+
+        /// <summary>
+        /// Gets all the text as string from image.
+        /// </summary>
+        /// <param name="imagePath">The image path.</param>
+        /// <returns></returns>
+        public static string GetStringFromImage(string imagePath)
+        {
+            var textValue = string.Empty;
+            if (string.IsNullOrWhiteSpace(imagePath) == false && System.IO.File.Exists(imagePath) == true)
+            {
+                try
+                {
+                    using (var engine = new TesseractEngine(@"./tessdata", "eng", EngineMode.Default))
+                    {
+                        using (var img = Pix.LoadFromFile(imagePath))
+                        {
+                            using (var page = engine.Process(img))
+                            {
+                                textValue = page.GetText();
+                            }
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Unexpected Error: " + ex.Message);
+                }
+            }
+            return textValue;
+        }
+
+        /// <summary>
+        /// Gets all the numbers seperately from image.
+        /// </summary>
+        /// <param name="imagePath">The image path.</param>
+        /// <returns></returns>
+        public static List<int> GetAllNumbersFromImage(string imagePath)
+        {
+            var numbersList = new List<int>();
+            if (string.IsNullOrWhiteSpace(imagePath) == false && System.IO.File.Exists(imagePath) == true)
+            {
+                try
+                {
+                    using (var engine = new TesseractEngine(@"./tessdata", "eng", EngineMode.Default))
+                    {
+                        using (var img = Pix.LoadFromFile(imagePath))
+                        {
+                            using (var page = engine.Process(img))
+                            {
+                                if (string.IsNullOrWhiteSpace(page.GetText()) == false)
+                                {
+                                    page.GetText()
+                                        .Split(' ')
+                                        .ToList()
+                                        .ForEach(text =>
+                                                {
+                                                    if (string.IsNullOrWhiteSpace(text) == false)
+                                                    {
+                                                        var numberValue = 0;
+                                                        text = regex.Replace(text, string.Empty);                               //remove non alpha numeric characters
+                                                        text = text.ToLowerInvariant().Replace('i', '1').Replace('o', '0');    //to fix wrong interpretation
+                                                        if (int.TryParse(text, out numberValue) == false)
+                                                        {
+                                                            Console.WriteLine("Unable to process the file : " + imagePath);
+                                                        }
+                                                        else
+                                                        {
+                                                            numbersList.Add(numberValue);
+                                                        }
+                                                    }
+                                                });
+                                }
+                            }
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Unexpected Error: " + ex.Message);
+                }
+            }
+            return numbersList;
+        }
+    }
+}
